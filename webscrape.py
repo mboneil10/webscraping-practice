712cfd307b3606ec5418d53515fba58e1ca01287
Molly O'Neil <mbethoneil@gmail.com>
2020-07-24 12:18:29 -0400

    Initial commit

---
+++ webscrape.py
@@ -0,0 +1,12 @@
+# Steps below are from https://www.edureka.co/blog/web-scraping-with-python/
+# Find the URL that you want to scrape
+    # Look at the website's `/robots.txt` file to determine if the website allows web scraping.
+import pandas as pd
+import selenium as sel
+import beautifulsoup as bs
+
+# Inspecting the Page
+# Find the data you want to extract
+# Write the code
+# Run the code and extract the data
+# Store the data in the required format
