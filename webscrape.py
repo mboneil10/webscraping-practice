8f5d438332baf04eb1b3f7eb1d5892436524d61a
Molly O'Neil <mbethoneil@gmail.com>
2020-07-24 12:33:18 -0400

    Make the steps clearer

--- webscrape.py
+++ webscrape.py
@@ -1,12 +1,29 @@
 # Steps below are from https://www.edureka.co/blog/web-scraping-with-python/
-# Find the URL that you want to scrape
+# 1 *** Find the URL that you want to scrape
     # Look at the website's `/robots.txt` file to determine if the website allows web scraping.
-import pandas as pd
-import selenium as sel
-import beautifulsoup as bs
 
-# Inspecting the Page
-# Find the data you want to extract
-# Write the code
-# Run the code and extract the data
-# Store the data in the required format
+# 2 *** Inspecting the Page
+    # Look at the specific divs by Inspecting the page
+
+# 3 *** Find the data you want to extract
+
+# 4 *** Write the code
+# For testing on the web
+from selenium import webdriver
+# Parses HTML and XML
+from beautifulsoup4 import BeautifulSoup
+#
+import pandas as pd
+
+# Set the webdriver to use Chrome browser. (This really doesn't matter to me).
+driver = webdriver.Chrome("/usr/lib/chromium-browser/chromedriver")
+
+products = []
+prices = []
+ratings = []
+
+# This is the page we are extracting data from / where we "open" the page
+driver.get("<a href="https://www.flipkart.com/laptops/">https://www.flipkart.com/laptops/</a>~buyback-guarantee-on-laptops-/pr?sid=6bo%2Cb5g&uniq")
+
+# 5 *** Run the code and extract the data
+# 6 *** Store the data in the required format
